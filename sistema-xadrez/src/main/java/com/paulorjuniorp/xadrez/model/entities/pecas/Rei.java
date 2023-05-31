12e3e79415c7170322126a2b66170e5acf37c8e9
paulorjuniorp <paulorjuniorp@gmail.com>
2023-05-31 07:15:30 -0300

    Implementação dos movimentos possíveis do rei

--- sistema-xadrez/src/main/java/com/paulorjuniorp/xadrez/model/entities/pecas/Rei.java
+++ sistema-xadrez/src/main/java/com/paulorjuniorp/xadrez/model/entities/pecas/Rei.java
@@ -1,5 +1,6 @@
 package com.paulorjuniorp.xadrez.model.entities.pecas;
 
+import com.paulorjuniorp.tabuleiro.model.entities.Posicao;
 import com.paulorjuniorp.tabuleiro.model.entities.Tabuleiro;
 import com.paulorjuniorp.xadrez.model.entities.PecaXadrez;
 import com.paulorjuniorp.xadrez.model.enums.Color;
@@ -14,9 +15,64 @@ public class Rei extends PecaXadrez {
         return "K";
     }
 
+    private boolean podeMover(Posicao posicao){
+        PecaXadrez pecaXadrez = (PecaXadrez) getTabuleiro().peca(posicao);
+        return pecaXadrez == null || pecaXadrez.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] movimentosPossiveis() {
         boolean[][] matrizAux = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+        Posicao posicaoAux = new Posicao(0,0);
+
+        // Acima
+        posicaoAux.setValores(posicao.getLinha() - 1, posicao.getColuna());
+        if (getTabuleiro().existePecaPosicao(posicaoAux) && podeMover(posicaoAux)){
+            matrizAux[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+        }
+
+        // Abaixo
+        posicaoAux.setValores(posicao.getLinha() + 1, posicao.getColuna());
+        if (getTabuleiro().existePecaPosicao(posicaoAux) && podeMover(posicaoAux)){
+            matrizAux[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+        }
+
+        // Esquerda
+        posicaoAux.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+        if (getTabuleiro().existePecaPosicao(posicaoAux) && podeMover(posicaoAux)){
+            matrizAux[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+        }
+
+        // Direita
+        posicaoAux.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+        if (getTabuleiro().existePecaPosicao(posicaoAux) && podeMover(posicaoAux)){
+            matrizAux[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+        }
+
+        // Noroeeste
+        posicaoAux.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+        if (getTabuleiro().existePecaPosicao(posicaoAux) && podeMover(posicaoAux)){
+            matrizAux[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+        }
+
+        // Nordeste
+        posicaoAux.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+        if (getTabuleiro().existePecaPosicao(posicaoAux) && podeMover(posicaoAux)){
+            matrizAux[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+        }
+
+        // Sudoete
+        posicaoAux.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+        if (getTabuleiro().existePecaPosicao(posicaoAux) && podeMover(posicaoAux)){
+            matrizAux[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+        }
+
+        // Sudeste
+        posicaoAux.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+        if (getTabuleiro().existePecaPosicao(posicaoAux) && podeMover(posicaoAux)){
+            matrizAux[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+        }
 
         return matrizAux;
     }
